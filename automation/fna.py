fde4dcd682478b52aba5db313186a30998b55e21
Ben Woo <iambenwoo@outlook.com>
2025-04-25 00:04:38 +0800

    Finetune Numpad

--- automation/fna.py
+++ automation/fna.py
@@ -2,24 +2,59 @@ from .utils import teardown_driver
 from .login import Login
 from .fna_helper import FNAHelpers
 
-
 class FNA:
-
     def perform_fna(self, map):
         driver = Login.perform_login(map)
+        overlay_seq = 2
+        num_pad_seq = 2
 
         try:
+            # Start new FNA
             FNAHelpers.start_new_fna(driver)
-            FNAHelpers.set_marital_status(driver, map)
-            FNAHelpers.set_number_of_dependents(driver, map)
+
+            # Set retirement age
+            overlay_seq += 1
+            num_pad_seq = 3
+            FNAHelpers.set_retirement_age_num_pad(driver, map, overlay_seq, num_pad_seq)
+
+            overlay_seq += 1
+            num_pad_seq = 2
+            FNAHelpers.set_extra_saving(driver, map, overlay_seq, num_pad_seq)
+
+            overlay_seq += 1
+            num_pad_seq = 2
+            FNAHelpers.set_extra_life(driver, map, overlay_seq, num_pad_seq)
+
+            overlay_seq += 1
+            num_pad_seq = 2
+            FNAHelpers.set_extra_medical(driver, map, overlay_seq, num_pad_seq)
+        
+            # Set marital status
+            overlay_seq += 1
+            FNAHelpers.set_marital_status(driver, map, overlay_seq)
+
+            # Set education level
+            overlay_seq += 1
+            FNAHelpers.set_education_level(driver, map, overlay_seq)
+
+            # Set number of dependents
+            overlay_seq += 1
+            FNAHelpers.set_number_of_dependents(driver, map, overlay_seq)
+
+            # Set occupation
             FNAHelpers.set_occupation(driver, map)
-            FNAHelpers.set_education_level(driver, map)
-            FNAHelpers.set_retirement_age(driver, map)
-            FNAHelpers.set_anb(driver, map)
-            #FNAHelpers.set_extra_life(driver, map)
+
+            
+            # Set ANB
+            overlay_seq += 1
+            FNAHelpers.set_anb(driver, map, overlay_seq)
+
+
+
+            # Input English name and mobile number
             FNAHelpers.input_english_name(driver, map)
-            # Uncomment the line below if mobile number input is required
-            # FNAHelpers.input_mobile_number(driver, map)
+            # FNAHelpers.input_mobile_number(driver, map)  # Uncomment if needed
+
         except Exception as e:
             print(f"ERROR: {str(e)}")
             raise
--- automation/fna_helper.py
+++ automation/fna_helper.py
@@ -11,31 +11,25 @@ class XPath:
     GIVEN_NAME_INPUT = "//div[@id='givenName']/div[2]/ion-input/input"
     MOBILE_NUMBER_INPUT = "//div[@id='mobile']/div[2]/ion-input/input"
     NUMBER_OF_DEPENDENT_SELECT = "(.//*[normalize-space(text()) and normalize-space(.)='受養人數目'])[1]/following::ion-select[1]"
-    NUMBER_OF_DEPENDENT_NIL = "//ion-popover[@id='ion-overlay-4']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item/ion-radio"
-    NUMBER_OF_DEPENDENT_1_3 = "//ion-popover[@id='ion-overlay-4']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[2]/ion-radio"
-    NUMBER_OF_DEPENDENT_4_6 = "//ion-popover[@id='ion-overlay-4']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[3]/ion-radio"
-    NUMBER_OF_DEPENDENT_7_ABOVE = "//ion-popover[@id='ion-overlay-4']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[4]/ion-radio"
+    NUMBER_OF_DEPENDENT = "//ion-popover[@id='ion-overlay-{seq}']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item{option}/ion-radio"
     MARITAL_STATUS_SELECT = "(.//*[normalize-space(text()) and normalize-space(.)='婚姻狀況'])[1]/following::ion-select[1]"
-    MARITAL_STATUS_SINGLE = "//ion-popover[@id='ion-overlay-3']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item/ion-radio"
-    MARITAL_STATUS_MARRIED = "//ion-popover[@id='ion-overlay-3']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[2]/ion-radio"
-    MARITAL_STATUS_DIVORCED = "//ion-popover[@id='ion-overlay-3']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[3]/ion-radio"
-    MARITAL_STATUS_WIDOWED = "//ion-popover[@id='ion-overlay-3']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[4]/ion-radio"
+    MARITAL_STATUS = "//ion-popover[@id='ion-overlay-{seq}']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item{option}/ion-radio"
     OCCUPATION_SELECT = "(.//*[normalize-space(text()) and normalize-space(.)='職業'])[1]/following::div[4]"
     OCCUPATION_SELECT_EDUCATION = "//*/text()[normalize-space(.)='教育']/parent::*"
     OCCUPATION_SELECT_EDUCATION_PRINCIPLE = "//*/text()[normalize-space(.)='校長']/parent::*"
     EDUCATION_LEVEL_SELECT = "(.//*[normalize-space(text()) and normalize-space(.)='教育程度'])[1]/following::ion-select[1]"
-    EDUCATION_LEVEL_PRIMARY = "//ion-popover[@id='ion-overlay-5']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item/ion-radio"
-    EDUCATION_LEVEL_SECONDARY = "//ion-popover[@id='ion-overlay-5']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[2]/ion-radio"
-    EDUCATION_LEVEL_VOCATIONAL = "//ion-popover[@id='ion-overlay-5']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[3]/ion-radio"
-    EDUCATION_LEVEL_UNIVERSITY = "//ion-popover[@id='ion-overlay-5']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[4]/ion-radio"
+    EDUCATION_LEVEL = "//ion-popover[@id='ion-overlay-{seq}']/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item{option}/ion-radio"
     RETIREMENT_AGE_SELECT = "//div[@id='retirementAge']/div[2]/div/span"
     ANB_SELECT = "(.//*[normalize-space(text()) and normalize-space(.)='出生日期 (下次生日年齡)'])[1]/following::span[2]"
-    ANB_YEAR_SELECT = "//ion-modal[@id='ion-overlay-7']/div/li-ionic4-datepicker-modal/ion-content/ion-grid/ion-row/ion-col[2]/ion-grid/ion-row/ion-col[3]/ion-button"
+    ANB_YEAR_SELECT = "//ion-modal[@id='ion-overlay-{seq}']/div/li-ionic4-datepicker-modal/ion-content/ion-grid/ion-row/ion-col[2]/ion-grid/ion-row/ion-col[3]/ion-button"
     ANB_YEAR_VALUE = "//*/text()[normalize-space(.)='1997']/parent::*"
-    ANB_MONTH_SELECT = "//ion-modal[@id='ion-overlay-7']/div/li-ionic4-datepicker-modal/ion-content/ion-grid/ion-row/ion-col[2]/ion-grid/ion-row/ion-col/ion-button"
+    ANB_MONTH_SELECT = "//ion-modal[@id='ion-overlay-{seq}']/div/li-ionic4-datepicker-modal/ion-content/ion-grid/ion-row/ion-col[2]/ion-grid/ion-row/ion-col/ion-button"
     ANB_MONTH_VALUE = "//*/text()[normalize-space(.)='七月']/parent::*"
     ANB_DAY_VALUE = "//*/text()[normalize-space(.)='10']/parent::*"
-    ANB_CONFIRM = "//ion-modal[@id='ion-overlay-7']/div/li-ionic4-datepicker-modal/ion-footer/ion-toolbar/ion-grid/ion-row/ion-col[3]/ion-button"
+    ANB_CONFIRM = "//ion-modal[@id='ion-overlay-{seq}']/div/li-ionic4-datepicker-modal/ion-footer/ion-toolbar/ion-grid/ion-row/ion-col[3]/ion-button"
+    EXTRA_LIFE_SELECT = "//div[@id='extraLifeProtNeeds']/div[2]/div"
+    EXTRA_MEDICAL_SELECT = "//div[@id='extMedCrisisBeneNeeds']/div[2]/div"
+    EXTRA_SAVING_SELECT = "//div[@id='extReqNetSaving']/div[2]/div"
 
 
 class Column:
@@ -48,6 +42,9 @@ class Column:
     OCCUPATION_SELECT = "Occupation_Select"
     EDUCATION_LEVEL = "Education_Level"
     RETIREMENT_AGE = "Retirement_Age"
+    EXTRA_LIFE = "Extra_Life"
+    EXTRA_MEDICAL = "Extra_Medical"
+    EXTRA_SAVING = "Extra_Saving"
 
 
 class FNAHelpers:
@@ -73,7 +70,7 @@ class FNAHelpers:
             raise
 
     @staticmethod
-    def set_marital_status(driver, map):
+    def set_marital_status(driver, map, overlay_seq):
         try:
             marital_status = map.get(Column.MARITAL_STATUS)
             wait_and_click(driver, XPath.MARITAL_STATUS_SELECT)
@@ -81,39 +78,43 @@ class FNAHelpers:
             wait_and_click(driver, XPath.MARITAL_STATUS_SELECT)
 
             marital_status_options = {
-                "single": XPath.MARITAL_STATUS_SINGLE,
-                "married": XPath.MARITAL_STATUS_MARRIED,
-                "divorced": XPath.MARITAL_STATUS_DIVORCED,
-                "widowed": XPath.MARITAL_STATUS_WIDOWED,
+                "single": "",
+                "married": "[2]",
+                "divorced": "[3]",
+                "widowed": "[4]",
             }
 
             if marital_status not in marital_status_options:
                 raise ValueError(f"Invalid marital status: {marital_status}")
 
-            wait_and_click(driver, marital_status_options[marital_status])
+            wait_and_click(driver, XPath.MARITAL_STATUS.format(
+                seq=overlay_seq, option=marital_status_options[marital_status]))
             time.sleep(1)
         except Exception as e:
             print(f"ERROR: Failed to set marital status - {str(e)}")
             raise
 
     @staticmethod
-    def set_number_of_dependents(driver, map):
+    def set_number_of_dependents(driver, map, overlay_seq):
         try:
             number_of_dependents = map.get(Column.NUMBER_OF_DEPENDENTS)
             wait_and_click(driver, XPath.NUMBER_OF_DEPENDENT_SELECT)
+            time.sleep(1)
+            wait_and_click(driver, XPath.NUMBER_OF_DEPENDENT_SELECT)
 
-            dependent_options = {
-                "nil": XPath.NUMBER_OF_DEPENDENT_NIL,
-                "1-3": XPath.NUMBER_OF_DEPENDENT_1_3,
-                "4-6": XPath.NUMBER_OF_DEPENDENT_4_6,
-                "7-above": XPath.NUMBER_OF_DEPENDENT_7_ABOVE,
+            number_of_dependents_options = {
+                "nil": "",
+                "1-3": "[2]",
+                "4-6": "[3]",
+                "7-above": "[4]",
             }
 
-            if number_of_dependents not in dependent_options:
+            if number_of_dependents not in number_of_dependents_options:
                 raise ValueError(
                     f"Invalid number of dependents: {number_of_dependents}")
 
-            wait_and_click(driver, dependent_options[number_of_dependents])
+            wait_and_click(driver, XPath.NUMBER_OF_DEPENDENT.format(
+                seq=overlay_seq, option=number_of_dependents_options[number_of_dependents]))
             time.sleep(1)
         except Exception as e:
             print(f"ERROR: Failed to set number of dependents - {str(e)}")
@@ -144,51 +145,93 @@ class FNAHelpers:
             raise
 
     @staticmethod
-    def set_education_level(driver, map):
+    def set_education_level(driver, map, overlay_seq):
         try:
             education_level = map.get(Column.EDUCATION_LEVEL)
             wait_and_click(driver, XPath.EDUCATION_LEVEL_SELECT)
             time.sleep(1)
             wait_and_click(driver, XPath.EDUCATION_LEVEL_SELECT)
 
-            education_options = {
-                "primary": XPath.EDUCATION_LEVEL_PRIMARY,
-                "secondary": XPath.EDUCATION_LEVEL_SECONDARY,
-                "vocational": XPath.EDUCATION_LEVEL_VOCATIONAL,
-                "university": XPath.EDUCATION_LEVEL_UNIVERSITY,
+            education_level_options = {
+                "primary": "",
+                "secondary": "[2]",
+                "vocational": "[3]",
+                "university": "[4]",
             }
 
-            if education_level not in education_options:
+            if education_level not in education_level_options:
                 raise ValueError(f"Invalid education level: {education_level}")
 
-            wait_and_click(driver, education_options[education_level])
+            wait_and_click(driver, XPath.EDUCATION_LEVEL.format(
+                seq=overlay_seq, option=education_level_options[education_level]))
             time.sleep(1)
         except Exception as e:
             print(f"ERROR: Failed to select education - {str(e)}")
             raise
 
     @staticmethod
-    def set_retirement_age(driver, map):
+    def set_retirement_age_num_pad(driver, map, overlay_seq, num_pad_seq):
         try:
+            retirement_age = map.get(Column.RETIREMENT_AGE)
             wait_and_click(driver, XPath.RETIREMENT_AGE_SELECT)
-            NumPad.wait_and_click(driver, "ion-overlay-6", map.get(Column.RETIREMENT_AGE))
+            time.sleep(1)
+            wait_and_click(driver, XPath.RETIREMENT_AGE_SELECT)
+
+            NumPad.wait_and_click(driver, overlay_seq, num_pad_seq, retirement_age)
         except Exception as e:
             print(f"ERROR: Failed to set retirement age - {str(e)}")
             raise
 
     @staticmethod
-    def set_anb(driver, map):
+    def set_anb(driver, map, overlay_seq):
         try:
             wait_and_click(driver, XPath.ANB_SELECT)
             time.sleep(1)
             wait_and_click(driver, XPath.ANB_SELECT)
             time.sleep(1)
-            wait_and_click(driver, XPath.ANB_YEAR_SELECT)
+            wait_and_click(
+                driver, XPath.ANB_YEAR_SELECT.format(seq=overlay_seq))
             wait_and_click(driver, XPath.ANB_YEAR_VALUE)
-            wait_and_click(driver, XPath.ANB_MONTH_SELECT)
+            wait_and_click(
+                driver, XPath.ANB_MONTH_SELECT.format(seq=overlay_seq))
             wait_and_click(driver, XPath.ANB_MONTH_VALUE)
             wait_and_click(driver, XPath.ANB_DAY_VALUE)
-            wait_and_click(driver, XPath.ANB_CONFIRM)
+            wait_and_click(driver, XPath.ANB_CONFIRM.format(seq=overlay_seq))
         except Exception as e:
             print(f"ERROR: Failed to set ANB - {str(e)}")
+            raise
+
+    @staticmethod
+    def set_extra_life(driver, map, overlay_seq, num_pad_seq):
+        try:
+            extra_life = map.get(Column.EXTRA_LIFE)
+            wait_and_click(driver, XPath.EXTRA_LIFE_SELECT)
+            time.sleep(1)
+            wait_and_click(driver, XPath.EXTRA_LIFE_SELECT)        
+
+            NumPad.wait_and_click(driver, overlay_seq, num_pad_seq, extra_life)
+        except Exception as e:
+            print(f"ERROR: Failed to set extra life - {str(e)}")
+            raise
+
+    @staticmethod
+    def set_extra_medical(driver, map, overlay_seq, num_pad_seq):
+        try:
+            extra_medical = map.get(Column.EXTRA_MEDICAL)
+            wait_and_click(driver, XPath.EXTRA_MEDICAL_SELECT)
+            NumPad.wait_and_click(driver, overlay_seq, num_pad_seq, extra_medical)
+        except Exception as e:
+            print(f"ERROR: Failed to set extra medical - {str(e)}")
+            raise
+
+    @staticmethod
+    def set_extra_saving(driver, map, overlay_seq, num_pad_seq):
+        try:
+            extra_saving = map.get(Column.EXTRA_SAVING)
+            wait_and_click(driver, XPath.EXTRA_SAVING_SELECT)
+            time.sleep(1)
+            wait_and_click(driver, XPath.EXTRA_SAVING_SELECT)
+            NumPad.wait_and_click(driver, overlay_seq, num_pad_seq, extra_saving)
+        except Exception as e:
+            print(f"ERROR: Failed to set extra saving - {str(e)}")
             raise
